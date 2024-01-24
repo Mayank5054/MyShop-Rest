5a9fddbfd6a375acf145f6743cc9e2dfb5394bb2
Mayank5054 <mayanksheladiya49@gmail.com>
2024-01-24 17:40:40 +0530

    CORS Protection Added and tested with codepen

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const feedRoutes = require("./routes/feed");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods","GET");
+    res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization");
+    next();
+})
 app.use("/feed",feedRoutes);
+
 app.listen(5354,(req,res,next) => {
     console.log("server started !");
 });
--- controllers/feed.js
+++ controllers/feed.js
@@ -7,7 +7,10 @@ exports.getPosts = (req,res,next) =>{
 
 exports.postPosts = (req,res,next) =>{
     console.log("post posts called !");
+    console.log(req.body.f_name);
+    console.log(req.headers);
     res.status(200).json({
+        ...req.body,
         message:"success Post posts"
     })
 }
