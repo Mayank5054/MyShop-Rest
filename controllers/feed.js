5e8633278eb2de85f5008d90b216f5bce92f1ac2
Mayank5054 <mayanksheladiya49@gmail.com>
2024-01-24 17:45:30 +0530

    sample code to handle post requests added

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,8 +7,26 @@ exports.getPosts = (req,res,next) =>{
 
 exports.postPosts = (req,res,next) =>{
     console.log("post posts called !");
-    console.log(req.body.f_name);
-    console.log(req.headers);
+    // console.log(req.body.f_name);
+    // console.log(req.headers);
+/*** sample code to handle post data 
+//     fetch("http://localhost:5354/feed/posts",{
+//     method:"POST",
+//     body: JSON.stringify({
+//     name:"mayank"
+//   }),
+//     headers:{
+//       "Content-Type":"application/json"
+//     }
+//   })
+//   .then(result => result.json())
+//   .then(result => {
+//     console.log(result);
+//   })
+
+***/
+
+    console.log(req.body);
     res.status(200).json({
         ...req.body,
         message:"success Post posts"
